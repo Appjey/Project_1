ff648daac085c6d41825c8ff15851350cbeec9e3
Appjey <loikoanton@yandex.ru>
2021-02-12 22:20:45 +0300

    :D

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def task11(x, y, z):
     result = math.sqrt(a) + math.sqrt(b / c) - d / e
     print(f"{result:.2e}")
 
-
 def task12(x):
     if x < 173:
         print(f"{function1(x):.2e}")
@@ -23,7 +22,22 @@ def task12(x):
     if x >= 338:
         print(f"{function5(x):.2e}")
 
+def task13(n, m):
+    print(f"{sum1(1, 1, n, m) - 85 * sum2(1, n):.2e}")
 
+def task14(n):
+    a = 0.0
+    b = 0.0
+    result = 0
+    if n == 0:
+        result = 4
+    if n == 1:
+        result = 6
+    if n > 1:
+        a = a + (1/76) * task14(n - 1)**3
+        b = b + math.tan(task14(n-2))
+        result = a - b
+    return result
 def function1(x):
     return math.log(50 * pow(x, 7) + pow(x, 4)) + math.fabs(pow(math.e, x))
 
@@ -58,20 +72,6 @@ def sum2(i, n):
     result = 0.0
     for x in range(i, n + 1):
         result = result + (pow(x, 6) / 89 + 41 * pow(x, 5))
-    return result
-
-def task14(n):
-    a = 0.0
-    b = 0.0
-    result = 0
-    if n == 0:
-        result = 4
-    if n == 1:
-        result = 6
-    if n > 1:
-        a = a + (1/76) * task14(n - 1)**3
-        b = b + math.tan(task14(n-2))
-        result = a - b
     return result
 
 task11(35, 70, -44)
