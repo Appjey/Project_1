f682a2a89c8ef922a25a262b453a612edac36770
Appjey <loikoanton@yandex.ru>
2021-02-23 05:18:34 +0300

    :D

--- main2.py
+++ main2.py
@@ -40,27 +40,35 @@ def task21(a, b, c, d, e):
 
 
 def task23(a):
-    #Splits, rounds and sets result to the cell
+    # Splits, rounds and sets result to the cell
     for i in range(0, len(a)):
         if a[i][0] is not None:
             b = a[i][0].split('|')
             a[i][0] = round(float(b[0]), 2)
             a[i][1] = b[1]
 
-    #Removes all empty cells and duplicates
+    # Removes all empty cells and duplicates
     for i in range(0, 3 + 1):
         a[i] = [item for item in a[i] if item is not None]
         a[i] = remove_d(a[i])
 
-    #Removes empty sub-array
+    # Removes empty sub-array
     for y in range(len(a)):
         if not a[y]:
             del a[y]
             break
-    #Transposes the matrix
-    return [[a[y][x] for y in range(len(a))] for x in range(len(a[0]))]
+    # Transposes the matrix
+    transpose = [[a[y][x] for y in range(len(a))] for x in range(len(a[0]))]
 
-#remove None and duplicates
+    # Replaces / by dot
+    for x in range(len(transpose[0])):
+        transpose[len(a) - 1][x] = transpose[len(a) - 1][x].replace('/', '.')
+        bb = transpose[len(a) - 1][x].split('.', 3)
+        transpose[len(a) - 1][x] = bb[0] + '.' + bb[1] + '.' + bb[2].replace(bb[2][0], '', 1).replace(bb[2][1], '', 1)
+    return transpose
+
+
+# remove None and duplicates
 def remove_d(a=[]):
     for i in range(0, len(a)):
         if a[i] == a[i - 1]:
@@ -68,9 +76,13 @@ def remove_d(a=[]):
     return a
 
 
-#print(task21(2009, 'haml', 1993, 2008, 1991))
-#print(task21(1973, 'pike', 1993, 1969, 1966))
+print(task21(2009, 'haml', 1993, 2008, 1991))
+print(task21(1973, 'pike', 1993, 1969, 1966))
 print(task23([['0.608|Айдар З. Цодберг', None, None, '25/04/2000', '25/04/2000'],
               [None, None, None, None, None],
               ['0.460|Марат В. Засуфин', None, None, '19/12/1999', '19/12/1999'],
               ['0.924|Арсен Л. Сикко', None, None, '20/02/2001', '20/02/2001']]))
+print(task23([['0.751|Артур С. Шувев', None, None, '09/03/2002', '09/03/2002'],
+              [None, None, None, None, None],
+              ['0.453|Михаил У. Буцук', None, None, '04/07/2002', '04/07/2004'],
+              ['0.952|Ростислав И. Римов', None, None, '28/11/2004', '28/11/2004']]))
