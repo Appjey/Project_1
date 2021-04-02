13d76f4cd8ca40cc02e4c6f471bab35d4f3c1251
Appjey <43535680+Appjey@users.noreply.github.com>
2021-04-03 00:23:55 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,46 +1,56 @@
-def task21(a, b, c, d, e):
-    if e == 1966:
+def f21(a):
+    if a[4] == 1966:
         return 12
-    if e == 1975:
+    if a[4] == 1975:
         return 11
-    if e == 1991:
-        if c == 1993:
-            if d == 2016:
-                if b == 'pike':
+    if a[4] == 1991:
+        if a[2] == 1993:
+            if a[3] == 2016:
+                if a[1] == 'pike':
                     return 0
-                if b == 'haml':
+                if a[1] == 'haml':
                     return 1
-            if d == 2008:
-                if b == 'pike':
+            if a[3] == 2008:
+                if a[1] == 'pike':
                     return 2
-                if b == 'haml':
+                if a[1] == 'haml':
                     return 3
-            if d == 1969:
-                if b == 'pike':
+            if a[3] == 1969:
+                if a[1] == 'pike':
                     return 4
-                if b == 'haml':
+                if a[1] == 'haml':
                     return 5
-        if c == 1998:
-            if d == 2016:
+        if a[2] == 1998:
+            if a[3] == 2016:
                 return 6
-            if d == 2008:
-                if a == 2009:
+            if a[3] == 2008:
+                if a[0] == 2009:
                     return 7
-                if a == 1976:
+                if a[0] == 1976:
                     return 8
-                if a == 1973:
+                if a[0] == 1973:
                     return 9
-            if d == 1969:
+            if a[3] == 1969:
                 return 10
     return 0
 
+def f22(x):
+    a = x & 0b00000000000000000000000000000001
+    b = x & 0b00000000000000000011111111111110
+    c = x & 0b00001111111111111110000000000000
+    d = x & 0b11111000000000000000000000000000
+    b = b << 19
+    c = c >> 7
+    d = d >> 26
+    a = a
+    return hex(b|c|d|a)
 
-def task23(a):
+def f23(a):
     # Splits, rounds and sets result to the cell
     for i in range(0, len(a)):
         if a[i][0] is not None:
             b = a[i][0].split('|')
-            a[i][0] = round(float(b[0]), 2)
+            a[i].insert(0, str(round(float(b[0]), 2)))
             a[i][1] = b[1]
 
     # Removes all empty cells and duplicates
@@ -49,7 +59,7 @@ def task23(a):
         a[i] = remove_d(a[i])
 
     # Removes empty sub-array
-    while len(a[0]) != len(a):
+    for i in range(len(a[0])):
         for y in range(len(a)):
             if not a[y]:
                 del a[y]
@@ -58,13 +68,17 @@ def task23(a):
     # Transposes the matrix
     transpose = [[a[y][x] for y in range(len(a))] for x in range(len(a[0]))]
 
+    #Replace Full name by short
+    for x in range(len(transpose[1])):
+        ss = transpose[1][x].split(" ", 1)
+        transpose[1][x] = ss[0][0] + '.' + ss[1]
+
     # Replaces / by dot
     for x in range(len(transpose[0])):
         transpose[len(a) - 1][x] = transpose[len(a) - 1][x].replace('/', '.')
         bb = transpose[len(a) - 1][x].split('.', 3)
         transpose[len(a) - 1][x] = bb[0] + '.' + bb[1] + '.' + bb[2].replace(bb[2][0], '', 1).replace(bb[2][1], '', 1)
     return transpose
-
 
 # remove None and duplicates
 def remove_d(a=None):
@@ -74,16 +88,3 @@ def remove_d(a=None):
         if a[i] == a[i - 1]:
             del a[i - 1]
     return a
-
-
-print(task21(2009, 'haml', 1993, 2008, 1991))
-print(task21(1973, 'pike', 1993, 1969, 1966))
-print(task23([['0.608|Айдар З. Цодберг', None, None, '25/04/2000', '25/04/2000'],
-              [None, None, None, None, None],
-              ['0.460|Марат В. Засуфин', None, None, '19/12/1999', '19/12/1999'],
-              ['0.924|Арсен Л. Сикко', None, None, '20/02/2001', '20/02/2001'],
-              [None, None, None, None, None]]))
-print(task23([['0.751|Артур С. Шувев', None, None, '09/03/2002', '09/03/2002'],
-              [None, None, None, None, None],
-              ['0.453|Михаил У. Буцук', None, None, '04/07/2002', '04/07/2004'],
-              ['0.952|Ростислав И. Римов', None, None, '28/11/2004', '28/11/2004']]))
