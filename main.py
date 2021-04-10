002a288cb202923040d0d254ea609b4ef0e63030
Appjey <loikoanton@yandex.ru>
2021-04-10 22:02:19 +0300

    Update and fix Pract_1 part

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import math
 
 
-def task11(x, y, z):
+def f11(x, y, z):
     a = math.sin(pow(y, 2) + 24 * pow(z, 8)) - pow(math.e, z)
     b = pow(x, 2) - pow(z, 6)
     c = math.log(y) - 22 * pow(x, 4) - 53
     d = 5 * pow(x, 4) - y / 32
     e = 27 * pow(x, 5) + math.log(x)
     result = math.sqrt(a) + math.sqrt(b / c) - d / e
-    print(f"{result:.2e}")
+    return result
 
 
-def task12(x):
+def f12(x):
     if x < 173:
-        print(f"{function1(x):.2e}")
+        return function1(x)
     if (x >= 173) and (x < 184):
-        print(f"{function2(x):.2e}")
+        return function2(x)
     if (x >= 184) and (x < 241):
-        print(f"{function3(x):.2e}")
+        return function3(x)
     if (x >= 241) and (x < 338):
-        print(f"{function4(x):.2e}")
+        return function4(x)
     if x >= 338:
-        print(f"{function5(x):.2e}")
+        return function5(x)
 
 
-def task13(n, m):
-    print(f"{sum1(1, 1, n, m) - 85 * sum2(1, n):.2e}")
+def f13(n, m):
+    return sum1(1, 1, n, m) - 85 * sum2(1, n)
 
 
-def task14(n):
+def f14(n):
     a = 0.0
     b = 0.0
     result = 0
@@ -37,8 +37,8 @@ def task14(n):
     if n == 1:
         result = 6
     if n > 1:
-        a = a + (1 / 76) * task14(n - 1) ** 3
-        b = b + math.tan(task14(n - 2))
+        a = a + (1 / 76) * f14(n - 1) ** 3
+        b = b + math.tan(f14(n - 2))
         result = a - b
     return result
 
@@ -78,7 +78,7 @@ def sum2(i, n):
     return result
 
 
-task11(35, 70, -44)
-task12(144)
-task13(18, 96)
-print(f"{task14(6):.2e}")
+print(f11(35, 70, -44))
+print(f12(144))
+print(f13(18, 96))
+print(f"{f14(6):.2e}")
