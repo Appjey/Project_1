eda217c245c3321eb584bf75f293b409989ed259
Appjey <loikoanton@yandex.ru>
2021-04-14 23:35:15 +0300

    making 3-th part ~50%

--- Part_Three/C32.py
+++ Part_Three/C32.py
@@ -1,26 +1,52 @@
 class C32:
-    state = []
 
     def __init__(self):
-        self.state = [None]
+        self.state = 'A'
 
     def send(self):
-        if self.state[-1] is None:
-            self.state[-1] = 0
+        if self.state == 'A':
+            self.state = 'B'
             return 0
-        elif self.state[-1] == 2:
-            self.state.append(2)
-            return 2
+        elif self.state == 'D':
+            self.state = 'B'
+            return 6
+        elif self.state == 'E':
+            self.state = 'F'
+            return 8
+        elif self.state == 'F':
+            self.state = 'G'
+            return 9
+        elif self.state == 'G':
+            self.state = 'H'
+            return 10
+        else:
+            raise RuntimeError
 
     def cull(self):
-        if self.state[-1] == 0:
-            self.state.append(2)
+        if self.state == 'B':
+            self.state = 'C'
             return 1
+        elif self.state == 'C':
+            self.state = 'D'
+            return 3
+        elif self.state == 'D':
+            self.state = 'G'
+            return 7
+        elif self.state == 'H':
+            self.state = 'A'
+            return 11
+        else:
+            raise RuntimeError
 
     def carve(self):
-        if self.state[-1] is None:
-            self.state[-1] = 0
-            return 0
-        elif self.state[-1] == 0:
-            self.state.append(2)
+        if self.state == 'C':
+            self.state = 'A'
+            return 4
+        elif self.state == 'B':
+            self.state = 'F'
             return 2
+        elif self.state == 'D':
+            self.state = 'E'
+            return 5
+        else:
+            raise RuntimeError
